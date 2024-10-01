82e67f0d1687168ee906a4b709ea5906353e622f
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-01 22:26:56 +0300

    adding data

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import Orom from "./assets/image/orom.png";
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: Orom,
+        title: "IRRECHA VIBE",
+        desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: Orom,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: Orom,
+        title: "POPULAR COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
+        bg: "fbf0f4",
+    },
+]
