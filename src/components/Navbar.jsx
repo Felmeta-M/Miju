2072989675384638ddb03231fcd875b4cc5bc5f9
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-03 19:28:14 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,25 @@ import styled from "styled-components";
 const Container = styled.div`
   height: 60px;
 `;
+
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
 `;
+
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
 `;
+
 const SearchContainer = styled.div`
   border: 1px solid lightgray;
   display: flex;
@@ -27,6 +31,7 @@ const SearchContainer = styled.div`
   margin-left: 25px;
   padding: 5px
 `;
+
 const Input = styled.input`
   border: none;
 `;
@@ -35,6 +40,7 @@ const Center = styled.div`
   flex: 1;
   text-align: center;
 `;
+
 const Logo = styled.h1`
   font-weight: bold;
 `;
