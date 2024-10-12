e0147fcee8ea0964b6208dbe3ffa52f1790d77bb
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-12 23:04:41 +0300

    announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    height: 30px;
-    background-color: #6b07f7;
-    color:white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    font-weight: 500;
+  height: 30px;
+  background-color: #2e2f5b;
+  // background-color: #505581;
+  // background-color: #f4d35e;
+  // background-color: #faf0ca;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
 `;
 
 const Announcement = () => {
-  return (
-    <Container> Free Shipping! on Orders Over 10,000 ETB</Container>
-  )
-}
+  return <Container> Free Shipping! on Orders Over 10,000 ETB</Container>;
+};
 
 export default Announcement;
