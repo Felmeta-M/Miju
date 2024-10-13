eda5d32dd66919add6fbdd87853eb19f1adf6949
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-13 22:49:12 +0300

    adding image

--- src/data.js
+++ src/data.js
@@ -1,23 +1,25 @@
 import Orom from "./assets/image/orom.png";
+import Or from "./assets/image/or.png";
+import Wing from "./assets/image/wing.png";
 
 export const sliderItems = [
     {
         id: 1,
         img: Orom,
         title: "IRRECHA VIBE",
-        desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
+        desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER ENJOY NEW DISCOUNT.",
         bg: "f5fafd",
     },
     {
         id: 2,
-        img: Orom,
+        img: Or,
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: Orom,
+        img: Wing,
         title: "POPULAR COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
         bg: "fbf0f4",
