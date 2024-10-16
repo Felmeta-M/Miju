314b6eecf636e0c1f5a9eab375c3889a29793998
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-16 20:51:42 +0300

    data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 import Orom from "./assets/image/orom.png";
 import Or from "./assets/image/or.png";
 import Wing from "./assets/image/wing.png";
+import Sura from "./assets/image/sura.jpg";
+import Sur from "./assets/image/sur.jpg";
+import Oromo from "./assets/image/oro.webp";
 
 export const sliderItems = [
     {
@@ -23,5 +26,58 @@ export const sliderItems = [
         title: "POPULAR COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ORDER.",
         bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: Oromo,
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: Sura,
+        title: "LONGWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: Sur,
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: Oromo,
+    },
+    {
+        id: 2,
+        img: Sura,
+    },
+    {
+        id: 3,
+        img: Sur,
+    },
+    {
+        id: 4,
+        img: Oromo,
+    },
+    {
+        id: 5,
+        img: Sura,
+    },
+    {
+        id: 6,
+        img: Sur,
+    },
+    {
+        id: 7,
+        img: Oromo,
+    },
+    {
+        id: 8,
+        img: Sura,
     },
 ]
