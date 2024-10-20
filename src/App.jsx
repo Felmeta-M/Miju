8b26440b4a057cb0e3f8603efe49eac8d08d6771
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-20 21:42:47 +0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-// import Home from './pages/Home'
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
   return <Product />;
