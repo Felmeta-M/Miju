a6a15872a1d5a0c1d150f1bf5a9c5e76e2f98e99
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-20 21:44:06 +0300

    responsive newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import { Send } from "@mui/icons-material";
 import styled from "styled-components";
+import { mobile, tablet } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -19,6 +20,8 @@ const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })};
+  ${tablet({ textAlign: "center" })};
 `;
 
 const InputContainer = styled.div`
@@ -28,6 +31,8 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "80%" })};
+  ${tablet({ width: "70%" })}
 `;
 
 const Input = styled.input`
