c8a8a5f7708a1301627277e2496c9905cfc1676f
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-24 21:01:48 +0300

    adding product

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/Home";
 // import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 
