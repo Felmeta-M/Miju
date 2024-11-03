cf13352f79b68a408c91f8aa67d6cf4f9aa48772
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-11-03 17:27:11 +0300

    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import Home from "./pages/Home";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
-// import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  const user = true;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
